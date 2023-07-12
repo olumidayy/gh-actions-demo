db818196f0956eead47388426960209acfa60c1d
olumide <olumidenwosu@gmail.com>
2023-07-12 14:13:28 +0100

    account for railway port

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 
-// App
+
 const app = express();
 app.get('/hello', (req, res) => {
   res.status(200).json({
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const { app } = require('./app');
 dotenv.config();
 
 const PORT = process.env.PORT || 8080;
-const HOST = process.env.HOST || 'localhost';
+const HOST = process.env.HOST || 'localhost'; 
 app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
 });
